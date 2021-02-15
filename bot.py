c94bab0428848621dcbb18dcfc31d1fd2e27e69b
zensimilia <zensimilia@gmail.com>
2021-02-15 13:06:24 +0300

    Refactoring `message.answer`

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from config import Config
 from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, executor, types
 
+# Configure constants from environment
 BOT_TOKEN = Config.BOT_TOKEN
 GEONAMES_USER = Config.GEONAMES_USER
 
@@ -26,7 +27,7 @@ async def send_about(message: types.Message):
     """
     This handler will be called when user sends `/about` command
     """
-    await bot.send_message(message.chat.id, "Hi! I'm the <b>Traffic Assistant Bot</b>.\nI will warn you about traffic jams on your route by schedule.")
+    await message.answer("Hi! I'm the <b>Traffic Assistant Bot</b>.\nI will warn you about traffic jams on your route by schedule.")
 
 
 @dp.message_handler(commands=['register'])
@@ -37,7 +38,7 @@ async def register_user(message: types.Message):
     user = db.register_user(message.from_user.id, message.from_user.username)
 
     if user:
-        await bot.send_message(message.chat.id, "You was succefull registered!")
+        await message.answer("You was succefull registered!")
 
 
 @dp.message_handler(commands=['routeadd'])
@@ -53,8 +54,8 @@ async def add_route(message: types.Message):
     # print(soup.get_text())
     # logging.debug(soup.find('div', class_='_mfyskr'))
     route = db.add_route(message.from_user.id, url, (name or ('',))[0])
-    text = "Маршрут добавлен." if route else "Что-то пошло не так!"
-    await bot.send_message(message.chat.id, text, parse_mode='html')
+    bot_answer = "Маршрут добавлен." if route else "Что-то пошло не так!"
+    await message.answer(bot_answer)
 
 
 @dp.message_handler(commands=['routes'])
@@ -64,18 +65,19 @@ async def all_routes(message: types.Message):
     """
     data = db.get_routes(message.from_user.id)
     for idx, route in enumerate(data, start=1):
-        text = f'{idx} route url: {route[2]}'
-        await bot.send_message(message.chat.id, text, disable_web_page_preview=True)
+        bot_answer = f'{idx} route url: {route[2]}'
+        await message.answer(bot_answer, disable_web_page_preview=True)
 
 
 @dp.message_handler(commands=['location'])
 async def get_user_location(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button_geo = types.KeyboardButton(
-        text="Отправить местоположение", request_location=True, callback_data=message)
+        text="Отправить местоположение",
+        request_location=True,
+        callback_data=message)
     keyboard.add(button_geo)
-    await bot.send_message(
-        message.chat.id, "Поделись местоположением, жалкий человечишка!", reply_markup=keyboard)
+    await message.answer("Отправьте местоположение для определения часового пояса или введите вручную (например: <code>UTC+3</code> или <code>Europe/Moscow</code>).", reply_markup=keyboard)
 
 
 @dp.message_handler(content_types=['location'])
@@ -95,7 +97,7 @@ async def set_user_timezone(message: types.Message):
 async def send_map_image(message: types.Message):
     timestamp = time.ctime()
     url = f'https://static-maps.yandex.ru/1.x/?l=map,trf&size=650,450&bbox=38.973102,45.021841~38.903821,45.075021&={timestamp}'
-    await bot.send_photo(message.chat.id, caption='MAP', photo=url)
+    await message.answer_photo(url, caption='MAP')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
