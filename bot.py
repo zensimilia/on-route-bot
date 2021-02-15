2e6a4906b7e82f357823570790233130ad3b3dd3
zensimilia <zensimilia@gmail.com>
2021-02-15 15:11:32 +0300

    Timezone handlers

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import time
 import logging
 import requests
 from config import Config
-from bs4 import BeautifulSoup
+from aiogram.dispatcher.filters import Text
 from aiogram import Bot, Dispatcher, executor, types
 
 # Configure constants from environment
@@ -48,11 +48,6 @@ async def add_route(message: types.Message):
     """
     db.register_user(message.from_user.id, message.from_user.username)
     url, *name = message.get_args().split()
-    # headers = {'User-Agent': 'Mozilla/5.0'}
-    # response = requests.get(url, headers=headers)
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    # print(soup.get_text())
-    # logging.debug(soup.find('div', class_='_mfyskr'))
     route = db.add_route(message.from_user.id, url, (name or ('',))[0])
     bot_answer = "Маршрут добавлен." if route else "Что-то пошло не так!"
     await message.answer(bot_answer)
@@ -69,15 +64,19 @@ async def all_routes(message: types.Message):
         await message.answer(bot_answer, disable_web_page_preview=True)
 
 
-@dp.message_handler(commands=['location'])
+@dp.message_handler(commands=['tz'])
 async def get_user_location(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    """
+    Set timezone for user.
+    """
+    keyboard = types.ReplyKeyboardMarkup(
+        row_width=1, resize_keyboard=True)
     button_geo = types.KeyboardButton(
         text="Отправить местоположение",
-        request_location=True,
-        callback_data=message)
+        request_location=True)
     keyboard.add(button_geo)
-    await message.answer("Отправьте местоположение для определения часового пояса или введите вручную (например: <code>UTC+3</code> или <code>Europe/Moscow</code>).", reply_markup=keyboard)
+    await message.answer("Отправьте своё местоположение для определения часового пояса или введите вручную.\n"
+                         "Например: <code>UTC+3</code> или <code>Europe/Moscow</code>.", reply_markup=keyboard)
 
 
 @dp.message_handler(content_types=['location'])
@@ -91,6 +90,16 @@ async def set_user_timezone(message: types.Message):
         await bot.send_message(message.chat.id, f'Your location: {timezone}', reply_markup=types.ReplyKeyboardRemove())
     else:
         await bot.send_message(message.chat.id, 'Cant get location.', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text(startswith=['utc'], ignore_case=True))
+async def set_timezone_by_utc(message: types.Message):
+    """
+    Set user timezone as UTC format.
+    """
+    timezone = message.text.upper().split(' ')[0]
+    db.add_user_timezone(message.from_user.id, timezone)
+    await message.answer(f'Ваш часовой пояс установлен как <code>{timezone}</code>', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(commands=['map'])
