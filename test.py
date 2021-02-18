5d9a34d8f18c3a4655e53b670febace3cd991850
Di M Dub <zensimilia@gmail.com>
2021-02-18 23:36:47 +0300

    Delete temporary files

--- test.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = 'https://go.2gis.com/rewrj'
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
-items = soup.find('div', class_='_mfyskr')
-print(items.div.text)
