89394130ff319913af9d6b873bf8f0a3dd9d97a8
zensimilia <zensimilia@gmail.com>
2021-03-24 12:49:34 +0300

    Change config class to use Pydantic BaseSettings for load and validation

--- app/config.py
+++ app/config.py
@@ -1,68 +1,24 @@
-import os
-from typing import Union, get_type_hints
-
-from dotenv import load_dotenv
-
-load_dotenv()  # reads the key-value pair from .env file and adds them to environment variable
+from pydantic import BaseSettings
 
 
-class AppConfigError(Exception):
-    pass
+class AppConfig(BaseSettings):
+    """
+    Global application configurations.
 
+    Variables will be loaded from the .env file. However, if
+    there is a shell environment variable having the same name,
+    that will take precedence.
+    """
 
-def _parse_bool(val: Union[str, bool]) -> bool:  # pylint: disable=E1136
-    return val if type(val) == bool else val.lower() in ['true', 'yes', '1']
-
-
-"""
-AppConfig class with required fields, default values,
-type checking, and typecasting for int and bool values.
-"""
-
-
-class AppConfig:
     DEBUG: bool = False
-    ENV: str = 'production'
     BOT_TOKEN: str
-    DB_FILE: str
+    DB_FILE: str = 'data.sqlite'
 
-    """
-    Map environment variables to class fields according to these rules:
-      - Field won't be parsed unless it has a type annotation
-      - Field will be skipped if not in all caps
-      - Class field and environment variable name are the same
-    """
+    class Config:
+        """Load variables from the dotenv file."""
 
-    def __init__(self, env):
-        for field in self.__annotations__:
-            if not field.isupper():
-                continue
-
-            # Raise AppConfigError if required field not supplied
-            default_value = getattr(self, field, None)
-            if default_value is None and env.get(field) is None:
-                raise AppConfigError('The {} field is required'.format(field))
-
-            # Cast env var value to expected type and raise AppConfigError on failure
-            try:
-                var_type = get_type_hints(AppConfig)[field]
-                if var_type == bool:
-                    value = _parse_bool(env.get(field, default_value))
-                else:
-                    value = var_type(env.get(field, default_value))
-
-                self.__setattr__(field, value)
-            except ValueError:
-                raise AppConfigError('Unable to cast value of "{}" to type "{}" for "{}" field'.format(
-                    env[field],
-                    var_type,
-                    field
-                )
-                )
-
-    def __repr__(self):
-        return str(self.__dict__)
+        env_file: str = '.env'
+        env_file_encoding: str = 'utf-8'
 
 
-# Expose Config object for app to import
-Config = AppConfig(os.environ)
+Config = AppConfig()
