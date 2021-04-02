d4368025cda9b6ed33d60bb52f43d39d9b1f7cef
zensimilia <zensimilia@gmail.com>
2021-04-02 15:55:20 +0300

    Fix adding schedule mechanism

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher.filters import Text
 from app.states import CreateRoute, CreateSchedule, SetTimezone
 from app.keyboards.inline_route import cd_routes
 from app.keyboards.settings import cd_settings
-from app.keyboards.inline_schedule import cd_schedules, cd_schedule_days
+from app.keyboards import inline_schedule
 
 
 log = logging.getLogger(__name__)
@@ -20,89 +20,65 @@ def register_errors_handler(dp: Dispatcher):
 def register_handlers_routes(dp: Dispatcher):
     """Register routes handlers in Dispatcher."""
     log.info('Configuring routes handlers...')
+    dp.register_message_handler(routes.route_list, commands='routes')
+    dp.register_message_handler(routes.route_add, commands='routeadd')
     dp.register_message_handler(
-        routes.route_list,
-        commands='routes'
-    )
-    dp.register_message_handler(
-        routes.route_add,
-        commands='routeadd'
+        routes.route_add_name, is_name=True, state=CreateRoute.name
     )
     dp.register_message_handler(
-        routes.route_add_name,
-        is_name=True,
-        state=CreateRoute.name
+        routes.route_add_url, is_url=True, state=CreateRoute.url
     )
     dp.register_message_handler(
-        routes.route_add_url,
-        is_url=True,
-        state=CreateRoute.url
+        routes.route_add_error, is_url=False, state=CreateRoute
     )
     dp.register_message_handler(
-        routes.route_add_error,
-        is_url=False,
-        state=CreateRoute
-    )
-    dp.register_message_handler(
-        routes.route_add_error,
-        is_name=False,
-        state=CreateRoute
+        routes.route_add_error, is_name=False, state=CreateRoute
     )
     dp.register_callback_query_handler(
-        routes.route_list,
-        cd_routes.filter(action='list')
+        routes.route_list, cd_routes.filter(action='list')
     )
     dp.register_callback_query_handler(
-        routes.route_select,
-        cd_routes.filter(action='select')
+        routes.route_select, cd_routes.filter(action='select')
     )
     dp.register_callback_query_handler(
-        routes.route_select,
-        cd_routes.filter(action='toggle')
+        routes.route_select, cd_routes.filter(action='toggle')
     )
     dp.register_callback_query_handler(
-        routes.route_show,
-        cd_routes.filter(action='show')
+        routes.route_show, cd_routes.filter(action='show')
     )
     dp.register_callback_query_handler(
-        routes.route_delete,
-        cd_routes.filter(action='delete')
+        routes.route_delete, cd_routes.filter(action='delete')
     )
     dp.register_callback_query_handler(
-        routes.route_select,
-        cd_routes.filter(action='delete_no')
+        routes.route_select, cd_routes.filter(action='delete_no')
     )
     dp.register_callback_query_handler(
-        routes.route_delete_confirm,
-        cd_routes.filter(action='delete_confirm')
+        routes.route_delete_confirm, cd_routes.filter(action='delete_confirm')
     )
 
 
 def register_handlers_schedules(dp: Dispatcher):
     """Register schedule handlers in Dispatcher."""
     log.info('Configuring schedule handlers...')
-    dp.register_message_handler(
+    dp.register_callback_query_handler(
         schedules.schedule_add_time,
-        is_time=True,
-        state=CreateSchedule.time
+        inline_schedule.cd_schedule_times.filter(),
+        state=CreateSchedule,
     )
     dp.register_callback_query_handler(
         schedules.schedule_add_days,
-        cd_schedule_days.filter(),
-        state=CreateSchedule.days
+        inline_schedule.cd_schedule_days.filter(),
+        state=CreateSchedule.days,
     )
     dp.register_message_handler(
-        schedules.schedule_add_error,
-        is_time=False,
-        state=CreateSchedule
+        schedules.schedule_add_error, is_time=False, state=CreateSchedule
     )
     dp.register_callback_query_handler(
-        schedules.schedule_list,
-        cd_routes.filter(action='schedule')
+        schedules.schedule_list, cd_routes.filter(action='schedule')
     )
     dp.register_callback_query_handler(
         schedules.schedule_add,
-        cd_schedules.filter(action='add')
+        inline_schedule.cd_schedules.filter(action='add'),
     )
 
 
@@ -111,24 +87,18 @@ def register_handlers_settings(dp: Dispatcher):
     log.info('Configuring settings handlers...')
     dp.register_message_handler(settings.settings_list, commands='settings')
     dp.register_callback_query_handler(
-        settings.settings_list,
-        cd_settings.filter(action='list')
+        settings.settings_list, cd_settings.filter(action='list')
     )
     dp.register_callback_query_handler(
-        settings.settings_tz,
-        cd_settings.filter(action='tz')
+        settings.settings_tz, cd_settings.filter(action='tz')
     )
     dp.register_callback_query_handler(
-        settings.settings_tz_change,
-        cd_settings.filter(action='tz-change')
+        settings.settings_tz_change, cd_settings.filter(action='tz-change')
     )
     dp.register_message_handler(
         settings.settings_tz_set,
-        Text(
-            startswith='UTC',
-            ignore_case=True
-        ),
-        state=SetTimezone
+        Text(startswith='UTC', ignore_case=True),
+        state=SetTimezone,
     )
     dp.register_message_handler(settings.settings_tz_error, state=SetTimezone)
 
@@ -141,7 +111,5 @@ def register_handlers_common(dp: Dispatcher):
     dp.register_message_handler(common.cmd_about, commands='about')
     dp.register_message_handler(common.cmd_cancel, commands='cancel', state='*')
     dp.register_message_handler(
-        common.cmd_cancel,
-        Text(equals='отмена', ignore_case=True),
-        state='*'
+        common.cmd_cancel, Text(equals='отмена', ignore_case=True), state='*'
     )
--- app/handlers/schedules.py
+++ app/handlers/schedules.py
@@ -3,8 +3,7 @@ import json
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from app.keyboards.inline_schedule import (cd_schedule_days, kb_schedule_days,
-                                           kb_schedule_list, kb_schedule_times)
+from app.keyboards import inline_schedule
 from app.models import Route, Schedule
 from app.states import CreateSchedule
 from app.utils import uchar
@@ -17,7 +16,7 @@ async def schedule_list(cb: types.CallbackQuery, callback_data: dict):
     schedules = route.schedules
     await cb.message.edit_text(
         f'Настройка уведомлений для маршрута <b>{route.name}</b>.',
-        reply_markup=kb_schedule_list(schedules, route_id)
+        reply_markup=inline_schedule.kb_schedule_list(schedules, route_id),
     )
     await cb.answer()
 
@@ -26,7 +25,7 @@ async def schedule_add(cb: types.CallbackQuery, callback_data: dict):
     await cb.message.edit_text(
         '<code>1/2</code> Пожалуйста, '
         'выберите желаемое время уведомления о маршруте.',
-        reply_markup=kb_schedule_times()
+        reply_markup=inline_schedule.kb_schedule_times(),
     )
     CreateSchedule.route_id = callback_data['route_id']
     await CreateSchedule.time.set()
@@ -34,38 +33,39 @@ async def schedule_add(cb: types.CallbackQuery, callback_data: dict):
 
 
 async def schedule_add_time(
-    cb: types.CallbackQuery,
-    callback_data: dict,
-    state: FSMContext
+    cb: types.CallbackQuery, callback_data: dict, state: FSMContext
 ):
-    time = callback_data['time']
-    await state.update_data(time=time)
+    await state.update_data(time=callback_data['time'])
     await CreateSchedule.next()
     await cb.message.edit_text(
         '<code>2/2</code> Теперь выберите дни, '
         'в которые надо получать уведомления.',
-        reply_markup=kb_schedule_days()
+        reply_markup=inline_schedule.kb_schedule_days(),
     )
 
 
-async def schedule_add_days(cb: types.CallbackQuery, state: FSMContext):
-    cb_data = cd_schedule_days.parse(cb.data)
-    await state.update_data(days=cb_data['days'])
+async def schedule_add_days(
+    cb: types.CallbackQuery, callback_data: dict, state: FSMContext
+):
+    """Save schedule data to database."""
+    await state.update_data(days=callback_data['days'])
     state_data = await state.get_data()
-    minute = state_data['time'].split(':')[-1]
-    hour = state_data['time'].split(':')[0]
+    time = state_data['time'].split(':')
     day_of_week = state_data['days']
     schedule = {
-        'minute': minute,
-        'hour': hour,
+        'hour': time[0],
+        'minute': time[1],
         'day_of_week': day_of_week,
     }
     route = Route.get_by_id(CreateSchedule.route_id)
     Schedule.create(route=route, schedule=json.dumps(schedule), is_active=True)
-    await state.finish()
     await cb.message.delete_reply_markup()
-    await cb.message.answer(f'Уведомление добавлено {uchar.OK_HAND}')
+    await cb.message.edit_text(
+        f'Уведомление добавлено {uchar.OK_HAND} '
+        '\nВернуться к списку маршрутов /routes'
+    )
     await cb.answer()
+    await state.finish()
 
 
 async def schedule_add_error(message: types.Message):
