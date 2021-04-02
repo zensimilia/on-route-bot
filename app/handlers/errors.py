e4ef45d761c65423eb1aad74dbd26b345feaa265
zensimilia <zensimilia@gmail.com>
2021-04-02 15:28:05 +0300

    Fix unused args for pylint

--- app/handlers/errors.py
+++ app/handlers/errors.py
@@ -4,8 +4,8 @@ from aiogram.types import Update
 log = logging.getLogger(__name__)
 
 
-async def errors_handler(update: Update, exception: Exception) -> bool:
-    """ Exceptions handler. Catches all exceptions within task factory tasks.
+async def errors_handler(_: Update, exception: Exception) -> bool:
+    """Exceptions handler. Catches all exceptions within task factory tasks.
     :param update: Object of incoming update.
     :param exception: An exception object.
     """
