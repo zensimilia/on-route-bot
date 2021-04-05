6f1576c3a8c5aa68feb232fcdc4c7b7503dd1666
zensimilia <zensimilia@gmail.com>
2021-04-05 14:14:04 +0300

    Debug env in logging

--- app/utils/log.py
+++ app/utils/log.py
@@ -2,14 +2,16 @@ import logging
 import logging.config
 from typing import Optional
 
+from app.config import Config
+
 DEFAULT_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'level': 'INFO',
-            'formatter': 'default'
+            'level': 'DEBUG' if Config.DEBUG else 'INFO',
+            'formatter': 'default',
         },
         'file': {
             'class': 'logging.handlers.RotatingFileHandler',
@@ -17,7 +19,7 @@ DEFAULT_CONFIG = {
             'formatter': 'default',
             'maxBytes': 1e6,
             'filename': 'store/log.txt',
-            'backupCount': 3
+            'backupCount': 3,
         },
     },
     'formatters': {
@@ -29,14 +31,14 @@ DEFAULT_CONFIG = {
         '': {
             'handlers': ['console', 'file'],
             'level': 'DEBUG',
-            'propagate': True
+            'propagate': True,
         }
-    }
+    },
 }
 
 
 def configure_logging(config: Optional[dict] = None) -> None:
-    """ Configure logging with dict of settings. By default takes config from
+    """Configure logging with dict of settings. By default takes config from
     global ``DEFAULT_CONFIG`` variable, using console and file handlers.
 
     :param config: Dict with logging configuration.
