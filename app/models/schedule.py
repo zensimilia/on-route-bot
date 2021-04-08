36c936b5db5a66c0a511969fcaca813d47c9e87d
zensimilia <zensimilia@gmail.com>
2021-04-08 15:57:46 +0300

    [wip] `get_active` in Schedule methods

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, LargeBinary
+from typing import List, TypeVar, Type, Optional
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, LargeBinary, select
+from sqlalchemy.orm import relationship
+
+
+from app.db import db_session
+
 from .base import Model
+from .route import Route
+
+T = TypeVar('T', bound='Schedule')
 
 
 class Schedule(Model):
@@ -7,8 +16,40 @@ class Schedule(Model):
 
     __tablename__ = 'schedules'
 
-    route_id: int = Column(
+    route_id = Column(
         Integer, ForeignKey('routes.id', ondelete='CASCADE'), nullable=False
     )
-    cron: str = Column(LargeBinary, nullable=False)
-    is_active: bool = Column(Boolean, nullable=False, default=True)
+    cron = Column(LargeBinary, nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    route = relationship(
+        'Route',
+        back_populates='schedules',
+        cascade='all, delete',
+        passive_deletes=True,
+        single_parent=True,
+    )
+
+    @classmethod
+    def get_active(cls: Type[T], route: Optional[Route] = None) -> List[T]:
+        if not route is None:
+            parent_cls = route.__class__
+            with db_session() as db:
+                return (
+                    db.query(cls)
+                    .where(
+                        (cls.is_active.__eq__(True))
+                        & (cls.route_id.__eq__(route.id))
+                    )
+                    .join(parent_cls)
+                    .where(parent_cls.is_active.is_(True))
+                    .all()
+                )
+        else:
+            with db_session() as db:
+                return (
+                    db.query(cls)
+                    .where(cls.is_active.__eq__(True))
+                    .join(Route)
+                    .where(Route.is_active.__eq__(True))
+                    .all()
+                )
